ff1236b08e7efdfeb5d540579346d20037e346c9
oman taupik <147501508+omantaupik@users.noreply.github.com>
2024-03-20 22:10:50 +0700

    list viewport for web and mobile

---
+++ screenview.js
@@ -0,0 +1,10 @@
+module.exports = {
+    dekstopview: {
+        viewportwidth: 1263,
+        viewportheight: 800
+    },
+    mobileview: {
+        viewportwidth: 375,
+        viewportheight: 667
+    },
+}
